854a7d6f60f412409be96c3d780354473fcd978d
one-summers-day <jiro.horikoshi1@gmail.com>
2022-01-24 17:53:11 -0600

    Fix routes to accomodate verification file for loader.io

--- server/router.js
+++ server/router.js
@@ -11,5 +11,8 @@ router.get('/products', getProductList);
 router.get('/products/:product_id', getProductInfo);
 router.get('/products/:product_id/styles', getProductStyles);
 router.patch('/products/sku/:sku_id', patchSkus);
+router.get('/loaderio-a66186c2eac8f9b8e18b6ae2d334619a.txt', (req, res) => {
+    res.sendFile('./loaderio-a66186c2eac8f9b8e18b6ae2d334619a.txt')
+})
 
 module.exports = router;
