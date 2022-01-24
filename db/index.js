fcc6eeb343f7557d19d3810192019ede68f256f6
one-summers-day <jiro.horikoshi1@gmail.com>
2022-01-24 08:09:34 -0600

    Fixed pool in index.js to allow for environment variable usage

--- db/index.js
+++ db/index.js
@@ -2,10 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
-  database: "products"
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT
 });
 
 module.exports = pool;
