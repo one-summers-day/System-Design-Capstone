8b3298515d747f798a7fb69974ba4cb21b8eee7f
one-summers-day <jiro.horikoshi1@gmail.com>
2022-01-24 08:20:07 -0600

    modify environment

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ const routes = require('./router.js')
 app.use(express.json());
 app.use('/', routes);
 
-app.set('port', process.env.PORT || 3000);
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Server listening on PORT: ', process.env.PORT || 3000);
+app.listen(process.env.PORT, function () {
+  console.log('Server listening on PORT: ', process.env.PORT);
 });
